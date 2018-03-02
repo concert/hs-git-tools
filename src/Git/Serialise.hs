adaf0328da3dba157aac16c2dd81b469c90ff2dd
Paul Weaver <paul@concertdaw.co.uk>
2018-03-02 12:15:30 +0000

    Serialise tree objects

--- src/Git/Serialise.hs
+++ src/Git/Serialise.hs
@@ -6,23 +6,26 @@ import Control.Monad (void)
 import Control.Monad.Fail (MonadFail(..))
 import Blaze.ByteString.Builder (Builder)
 import qualified Blaze.ByteString.Builder as Builder
-import Data.Attoparsec.ByteString.Char8 (char, decimal, signed, takeTill)
+import Data.Attoparsec.ByteString.Char8
+  (char, signed, takeTill, anyChar, decimal)
 import Data.Attoparsec.ByteString.Lazy
   ( Parser, Result(..), parse, string, endOfInput
-  , takeLazyByteString, many', take, anyWord8, (<?>))
+  , takeLazyByteString, many', many1, take, anyWord8, (<?>))
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.ByteString.Lazy as LBS
+import Data.Char (ord)
 import Data.Monoid ((<>))
-import Data.List (intercalate)
+import Data.List (intercalate, foldl')
 import Data.Proxy (Proxy(..))
 import qualified Data.Text as Text
 import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Data.Time
   (minutesToTimeZone, timeZoneMinutes, ZonedTime(..), zonedTimeToUTC)
 import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import System.Posix (CMode(..))
 
-import Git.Types (Blob(..), Commit(..), toZonedTime)
+import Git.Types (Blob(..), Tree(..), TreeRow(..), Commit(..), toZonedTime)
 import qualified Git.Types.Sha1 as Sha1
 import Git.Types.SizedByteString (SizedByteString)
 import qualified Git.Types.SizedByteString as SBS
@@ -42,8 +45,7 @@ class GitObject a where
       parseHeader :: Parser Integer
       parseHeader = do
         _ <- (string $ objectName $ Proxy @a) <?> "object name"
-        char_ ' '
-        decimal <* char '\NUL' <?> "object size"
+        char_ ' ' >> decimal <* char_ '\NUL' <?> "object size"
 
   objectName :: proxy a -> BS.ByteString
   objectBody :: a -> SizedByteString
@@ -54,6 +56,24 @@ instance GitObject Blob where
   objectBody = blobData
   objectParser size =
     Blob . SBS.takeFromLazyByteString size <$> takeLazyByteString
+
+
+instance GitObject Tree where
+  objectName _ = "tree"
+  objectBody = SBS.fromStrictByteString . Builder.toByteString .
+      mconcat . fmap rowB . unTree
+    where
+      sha1ByteStringB = b . Sha1.unSha1
+      fileModeB (CMode o) = b $ toOctBS o
+      nameB = b . encodeUtf8
+      rowB (TreeRow mode name sha1) =
+        fileModeB mode <> nameB name <> sha1ByteStringB sha1
+  objectParser _ = Tree <$> many1 rowP
+    where
+      fileModeP = CMode . fromIntegral <$> oct
+      nameP = decodeUtf8 <$> takeTill' (== '\NUL')
+      sha1ByteStringP = take 20 >>= Sha1.fromByteString
+      rowP = TreeRow <$> (fileModeP <* char ' ') <*> nameP <*> sha1ByteStringP
 
 
 instance GitObject Commit where
@@ -101,7 +121,7 @@ instance GitObject Commit where
         name <- decodeUtf8 . BS.init <$> takeTill (== '<')
         email <- decodeUtf8 <$> emailP
         char_ ' '
-        posixTime <- fromIntegral @Integer <$> decimal
+        posixTime <- fromIntegral @Int <$> decimal
         char_ ' '
         tz <- minutesToTimeZone <$> signed decimal
         char_ '\n'
@@ -119,5 +139,28 @@ takeTill' p = takeTill p <* anyWord8
 char_ :: Char -> Parser ()
 char_ = void . char
 
+satisfyMap :: String -> (Char -> Maybe a) -> Parser a
+satisfyMap errStr f = anyChar >>= maybe (fail errStr) return . f
+
+oct :: Parser Int
+oct = numberValue <$> many1 (satisfyMap "digit" digitToInt) <?> "octal"
+  where
+    digitToInt c = let dig = ord c - ord '0' in
+      if (fromIntegral dig :: Word) < 8 then Just dig else Nothing
+    numberValue = foldl' (\acc -> ((8 * acc) +)) 0
+
+
 b :: BS.ByteString -> Builder
 b = Builder.fromByteString
+
+toOctBS :: Integral a => a -> BS.ByteString
+toOctBS = BS.pack . fmap (fromIntegral . wordToDigit) . toWords
+  where
+    wordToDigit = (+ (fromIntegral $ ord '0'))
+    toWords = go []
+    go acc o = let (q, r) = quotRem o 8 in
+      case q of
+        0 -> r : acc
+        _ -> if q < 8
+                then q : r : acc
+                else go (r : acc) q
