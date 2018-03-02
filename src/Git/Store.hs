de796c5274be226518a4f1a23e706759c26b5419
Paul Weaver <paul@concertdaw.co.uk>
2018-03-02 13:56:57 +0000

    Actually hash the correct thing when storing objects
    Should have actually checked at what stage the hashing is done! This makes
    more sense too...

--- src/Git/Store.hs
+++ src/Git/Store.hs
@@ -4,26 +4,25 @@ import Codec.Compression.Zlib (compress, decompress)
 import qualified Data.ByteString.Lazy as LBS
 import System.Directory (renameFile, createDirectoryIfMissing)
 import System.FilePath.Posix ((</>))
-import System.IO
-  (openTempFile, openBinaryFile, hSeek, SeekMode(..), hClose, IOMode(..))
+import System.IO (openTempFile, openBinaryFile, hClose, IOMode(..))
 
 import Git.Serialise (GitObject(..))
 import Git.Types.Sha1 (Sha1, hashLazy, toHexString)
 import qualified Git.Types.SizedByteString as SBS
 
-storeObject :: GitObject a => FilePath -> a -> IO ()
-storeObject storePath obj = do
-  -- FIXME: could threshold on file size whether we can just run in memory? (I'm
-  -- just aware with all of this that we could be handling hundreds of megs)
+storeObject :: GitObject a => FilePath -> a -> IO Sha1
+storeObject storePath obj = let encoded = encodeObject obj in do
   (tmpPath, handle) <- openTempFile storePath "compressed_encoded_object"
-  LBS.hPut handle $ compress $ SBS.toLazyByteString $ encodeObject obj
-  hSeek handle AbsoluteSeek 0
-  sha1 <- hashLazy <$> LBS.hGetContents handle
+  LBS.hPut handle $ compress $ SBS.toLazyByteString $ encoded
   hClose handle
-  let (sha1Head, filename) = splitAt 2 $ toHexString sha1
+  -- FIXME: It might be good to try to parallelise the hashing with the file
+  -- writing:
+  let sha1 = hashLazy $ SBS.toLazyByteString encoded
+  let (sha1Head, filename) = splitAt 2 $ toHexString $ sha1
   let dirPath = storePath </> sha1Head
   createDirectoryIfMissing True dirPath
   renameFile tmpPath $ dirPath </> filename
+  return sha1
 
 -- We only let you get access to the retrieved objects in a context, because we
 -- have to have a hook for cleaning up the temporary uncompressed files.
