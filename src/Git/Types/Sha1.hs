b9ea5cc1997c2f211ed42c81db0620e5292821c5
Paul Weaver <paul@concertdaw.co.uk>
2018-03-04 15:27:32 +0000

    Expose sha1Size, make Sha1 an Ord instance

--- src/Git/Types/Sha1.hs
+++ src/Git/Types/Sha1.hs
@@ -1,5 +1,5 @@
 module Git.Types.Sha1
-  ( Sha1, unSha1, fromByteString
+  ( Sha1, unSha1, sha1Size, fromByteString
   , toHexString, fromHexString
   , hashLazy
   ) where
@@ -13,13 +13,16 @@ import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.ByteString.Lazy as LBS
 
-newtype Sha1 = Sha1 {unSha1 :: BS.ByteString} deriving (Eq)
+newtype Sha1 = Sha1 {unSha1 :: BS.ByteString} deriving (Eq, Ord)
 
 instance Show Sha1 where
   show sha1 = "sha1: " ++ toHexString sha1
 
+sha1Size :: Int
+sha1Size = 20
+
 fromByteString :: MonadFail m => BS.ByteString -> m Sha1
-fromByteString bs | BS.length bs == 20 = return $ Sha1 bs
+fromByteString bs | BS.length bs == sha1Size = return $ Sha1 bs
                   | otherwise = fail "sha1: fromByteString: bad digest length"
 
 toHexString :: Sha1 -> String
@@ -29,7 +32,6 @@ fromHexString :: MonadFail m => String -> m Sha1
 fromHexString s = case Base16.decode $ Char8.pack s of
   (bs, "") -> fromByteString bs
   _ -> fail $ "sha1: fromHexString: decodeError"
-
 
 hashLazy :: LBS.ByteString -> Sha1
 hashLazy = Sha1 . SHA1.hashlazy
