3d931cc18f160a416bf95815904aa539b6622341
Paul Weaver <paul@concertdaw.co.uk>
2018-03-04 15:26:45 +0000

    Use Tagged to assist with keep track of what object a Sha1 points to

--- src/Git/Store.hs
+++ src/Git/Store.hs
@@ -2,6 +2,7 @@ module Git.Store where
 
 import Codec.Compression.Zlib (compress, decompress)
 import qualified Data.ByteString.Lazy as LBS
+import Data.Tagged (Tagged(..))
 import System.Directory (renameFile, createDirectoryIfMissing)
 import System.FilePath.Posix ((</>))
 import System.IO (openTempFile, openBinaryFile, hClose, IOMode(..))
@@ -10,7 +11,7 @@ import Git.Serialise (GitObject(..))
 import Git.Types.Sha1 (Sha1, hashLazy, toHexString)
 import qualified Git.Types.SizedByteString as SBS
 
-storeObject :: GitObject a => FilePath -> a -> IO Sha1
+storeObject :: GitObject a => FilePath -> a -> IO (Tagged a Sha1)
 storeObject storePath obj = let encoded = encodeObject obj in do
   (tmpPath, handle) <- openTempFile storePath "compressed_encoded_object"
   LBS.hPut handle $ compress $ SBS.toLazyByteString $ encoded
@@ -22,12 +23,12 @@ storeObject storePath obj = let encoded = encodeObject obj in do
   let dirPath = storePath </> sha1Head
   createDirectoryIfMissing True dirPath
   renameFile tmpPath $ dirPath </> filename
-  return sha1
+  return $ Tagged sha1
 
 -- We only let you get access to the retrieved objects in a context, because we
 -- have to have a hook for cleaning up the temporary uncompressed files.
-retrieveObject :: GitObject a => FilePath -> Sha1 -> IO a
+retrieveObject :: GitObject a => FilePath -> Tagged a Sha1 -> IO a
 retrieveObject storePath sha1 =
-  let (sha1Head, filename) = splitAt 2 $ toHexString sha1 in
+  let (sha1Head, filename) = splitAt 2 $ toHexString $ unTagged sha1 in
   openBinaryFile (storePath </> sha1Head </> filename) ReadMode
   >>= LBS.hGetContents >>= decodeObject . decompress
