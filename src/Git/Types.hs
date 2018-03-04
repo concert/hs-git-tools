e4b1b5efa4244f45bf1e4e0b9de744bf18757e28
Paul Weaver <paul@concertdaw.co.uk>
2018-03-04 15:27:55 +0000

    Add a Version data type

--- src/Git/Types.hs
+++ src/Git/Types.hs
@@ -1,4 +1,6 @@
-module Git.Types (module X) where
+module Git.Types (module X, Version(..)) where
 
 import Git.Types.Sha1 as X
 import Git.Types.Objects as X
+
+data Version = Version1 | Version2 deriving (Show, Eq, Enum, Bounded)
