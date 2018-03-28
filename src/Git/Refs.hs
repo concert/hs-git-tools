6edb3f188aa116e4e46674a347bd86119c5d5eb1
Paul Weaver <paul@concertdaw.co.uk>
2018-03-28 15:30:00 +0100

    Open refs correctly
    By stripping whitespace and not faffing with length.

--- src/Git/Refs.hs
+++ src/Git/Refs.hs
@@ -5,6 +5,8 @@ import Control.Monad.Fail (MonadFail)
 import Control.Monad.IO.Class (MonadIO(..))
 import qualified Data.ByteString as BS
 import Data.Tagged (Tagged(..))
+import qualified Data.Text as Text
+import Data.Text.Encoding (decodeUtf8)
 import System.FilePath.Posix ((</>))
 import System.Directory (listDirectory)
 import System.IO (withBinaryFile, IOMode(..))
@@ -41,11 +43,9 @@ openRef
   :: (MonadIO m, MonadFail m, RefType rt)
   => Repo -> Ref rt -> m (Tagged Commit Sha1)
 openRef repo ref = let path = refTypePath repo (refType ref) </> unRef ref in do
-  liftIO (
-    withBinaryFile path ReadMode
-    -- FIXME: perhaps it's not the nicest parsing just to take the first
-    -- sha1Size bytes...
-    (fmap (Sha1.fromByteString . BS.take Sha1.sha1Size) . BS.hGetContents))
-  >>= either fail (return . Tagged)
+    liftIO (
+      withBinaryFile path ReadMode
+      (fmap (Sha1.fromHexText . Text.stripEnd . decodeUtf8) . BS.hGetContents))
+    >>= either fail (return . Tagged)
 
 data HEAD = HeadRef (Ref RtHead) | HeadCommit (Tagged Commit Sha1) deriving (Eq, Show)
