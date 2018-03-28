49185c0dd90a7d030fd702d78a62c22bf06c5aa0
Paul Weaver <paul@concertdaw.co.uk>
2018-03-28 15:28:48 +0100

    Add more Sha1 conversions

--- src/Git/Types/Sha1.hs
+++ src/Git/Types/Sha1.hs
@@ -1,6 +1,8 @@
 module Git.Types.Sha1
   ( Sha1, unSha1, sha1Size, fromByteString
+  , toHexByteString, fromHexByteString
   , toHexString, fromHexString
+  , toHexText, fromHexText
   , hashLazy, hashSbs
   ) where
 
@@ -12,6 +14,8 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Char8 as Char8
 import qualified Data.ByteString.Lazy as LBS
+import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 
 import Git.Types.SizedByteString (SizedByteString)
 import qualified Git.Types.SizedByteString as SBS
@@ -28,13 +32,25 @@ fromByteString :: MonadFail m => BS.ByteString -> m Sha1
 fromByteString bs | BS.length bs == sha1Size = return $ Sha1 bs
                   | otherwise = fail "sha1: fromByteString: bad digest length"
 
-toHexString :: Sha1 -> String
-toHexString = Char8.unpack . Base16.encode . unSha1
+fromHexByteString :: MonadFail m => BS.ByteString -> m Sha1
+fromHexByteString bs = case Base16.decode bs of
+  (bs', "") -> fromByteString bs'
+  _ -> fail $ "sha1: fromHexString: decodeError"
+
+toHexByteString :: Sha1 -> BS.ByteString
+toHexByteString = Base16.encode . unSha1
 
 fromHexString :: MonadFail m => String -> m Sha1
-fromHexString s = case Base16.decode $ Char8.pack s of
-  (bs, "") -> fromByteString bs
-  _ -> fail $ "sha1: fromHexString: decodeError"
+fromHexString = fromHexByteString . Char8.pack
+
+toHexString :: Sha1 -> String
+toHexString = Char8.unpack . toHexByteString
+
+fromHexText :: MonadFail m => Text -> m Sha1
+fromHexText = fromHexByteString . encodeUtf8
+
+toHexText :: Sha1 -> Text
+toHexText = decodeUtf8 . toHexByteString
 
 hashLazy :: LBS.ByteString -> Sha1
 hashLazy = Sha1 . SHA1.hashlazy
