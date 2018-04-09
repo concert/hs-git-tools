d6e98b9f301228e064b86559ad6a3ae2f6c05486
Paul Weaver <paul@concertdaw.co.uk>
2018-04-09 16:06:13 +0100

    Add support for Version2 and Version3 index files

--- src/Git/Index.hs
+++ src/Git/Index.hs
@@ -5,12 +5,13 @@
 
 module Git.Index where
 
-import Control.Monad (unless)
+import Control.Monad (unless, when)
 import Control.Monad.Except (MonadError(..))
 import Control.Monad.IO.Class (MonadIO(..))
-import Data.Attoparsec.ByteString (Parser, (<?>), string, endOfInput)
+import Data.Attoparsec.ByteString
+  (Parser, (<?>), string, endOfInput, many1, satisfy, takeTill)
 import Data.Attoparsec.Binary (anyWord16be, anyWord32be)
-import Data.Bits ((.&.), shiftR, testBit)
+import Data.Bits (Bits, (.&.), shiftR, testBit)
 import qualified Data.ByteString.Lazy as LBS
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -18,6 +19,7 @@ import Data.Monoid ((<>))
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text as Text
 import Data.Time.Clock.POSIX (POSIXTime, systemToPOSIXTime)
 import Data.Time.Clock.System (SystemTime(..))
@@ -81,9 +83,8 @@ openIndex path = do
   (versionNo, numEntries) <- either (throwError . ParseError) return $
     lazyParseOnly (headerP <* endOfInput) headBytes
   version <- versionFromWord32 versionNo
-  unless (version == Version4) $ throwError UnsupportedIndexVersion
   entries <- either (throwError . ParseError) return $
-    lazyParseOnly (indexEntriesP numEntries) content
+    lazyParseOnly (indexEntriesP version numEntries) content
   return $ Index version entries
 
 headerP :: Parser (Word32, Word32)
@@ -93,16 +94,17 @@ headerP = do
   numEntries <- anyWord32be
   return (version, numEntries)
 
-indexEntriesP :: Word32 -> Parser IndexEntries
-indexEntriesP = fmap Map.fromList . go ""
+indexEntriesP :: IndexVersion -> Word32 -> Parser IndexEntries
+indexEntriesP version = fmap Map.fromList . go ""
   where
     go _ 0 = return []
     go prevPath numEntries = do
-      entryData@((path, _), _) <- entryP prevPath
+      entryData@((path, _), _) <- entryP version prevPath
       (entryData:) <$> go path (numEntries - 1)
 
-entryP :: FilePathText -> Parser ((FilePathText, Stage), IndexEntry)
-entryP prevPath = do
+entryP
+  :: IndexVersion -> FilePathText -> Parser ((FilePathText, Stage), IndexEntry)
+entryP version prevPath = do
   ctime <- posixTimeP
   mtime <- posixTimeP
   devId <- CDev . fromIntegral <$> anyWord32be
@@ -112,10 +114,10 @@ entryP prevPath = do
   gid <- CGid <$> anyWord32be
   size <- anyWord32be
   sha1 <- sha1ByteStringP
-  (stage, flags) <- flagsP
-  -- NB: This is in version 4 only - support for prior versions would require
-  -- much more work!
-  path <- pathP prevPath
+  (stage, flags) <- flagsP version
+  path <- case version of
+        Version4 -> v4PathP prevPath
+        _ -> v1_3PathP
   return
     ( (path, stage)
     , IndexEntry ctime mtime devId inodeNo mode uid gid size sha1 flags)
@@ -129,12 +131,17 @@ posixTimeP = do
 fileModeP :: Parser FileMode
 fileModeP = anyWord32be >>= fileModeFromInt . fromIntegral
 
-flagsP :: Parser (Stage, Set Flag)
-flagsP = do
+flagsP :: IndexVersion -> Parser (Stage, Set Flag)
+flagsP version = do
     bits <- anyWord16be
     let assumeValid = flag AssumeValid $ testBit bits 15
     let continue = testBit bits 14
+    when (continue && version < Version3) $
+      fail $ "Extended flags bit unexpectedly set in " ++ show version
     let stage = fromIntegral $ shiftR bits 12 .&. 0b11
+    -- The name length info only appears to be useful for memory allocation
+    -- purposes:
+    -- let nameLen = lowMask bits 12  -- Version < 4
     if continue
       then (stage,) . (assumeValid <>) <$> extendedP
       else return (stage, assumeValid)
@@ -142,12 +149,20 @@ flagsP = do
     flag f b = if b then Set.singleton f else mempty
     extendedP = do
       bits <- anyWord16be
+      unless (lowMask bits 13 == 0) $
+        fail "Unexpected value in unused extended flags"
       return $
            (flag SkipWorkTree $ testBit bits 14)
         <> (flag IntentToAdd $ testBit bits 13)
 
-pathP :: FilePathText -> Parser FilePathText
-pathP prevPath = do
+v1_3PathP :: Parser FilePathText
+v1_3PathP = fmap decodeUtf8 $ takeTill (== 0) <* many1 (satisfy (== 0))
+
+v4PathP :: FilePathText -> Parser FilePathText
+v4PathP prevPath = do
   cut <- fromIntegral <$> chunkNumBeP
   new <- nullTermStringP
   return $ Text.dropEnd cut prevPath <> new
+
+lowMask :: (Bits a, Num a) => a -> Int -> a
+lowMask bits n = bits .&. 2 ^ n - 1
