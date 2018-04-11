2d846e05fc63d4581b31ba7ef7dde7165aaea79d
Paul Weaver <paul@concertdaw.co.uk>
2018-04-11 08:46:02 +0100

    Add skeleton Index.Parser test

---
+++ test/Git/Index/ParserSpec.hs
@@ -0,0 +1,10 @@
+module Git.Index.ParserSpec where
+
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Parser" $ do
+  it "should decode a real normal v2 tree" $ pending
+  it "should decode a real normal v3 tree" $ pending
+  it "should decode a real normal v4 tree" $ pending
+  it "should decode a real conflicting v2 tree" $ pending
