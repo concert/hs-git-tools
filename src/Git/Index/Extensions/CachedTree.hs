fb25206438d2b99c7b4acc55908ec9b0ad38ebd8
Paul Weaver <paul@concertdaw.co.uk>
2018-04-17 14:16:14 +0100

    Implement parsing for CachedTree and ResolveUndo extensions

--- src/Git/Index/Extensions/CachedTree.hs
+++ src/Git/Index/Extensions/CachedTree.hs
@@ -1,9 +1,38 @@
 module Git.Index.Extensions.CachedTree where
 
-import Git.Index.Extensions.Class (IndexExtension(..))
+import Data.Attoparsec.ByteString.Char8 (decimal)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Word
+import qualified Data.Text as Text
+import qualified System.Path as Path
 
-data CachedTree = CachedTree deriving (Show)
+import Git.Index.Extensions.Class (IndexExtension(..))
+import Git.Internal (takeFor, nullTermStringP, char_)
+import Git.Sha1 (Sha1, sha1HexParser)
+
+
+newtype CachedTree
+  = CachedTree (Map Path.RelFileDir CachedTreeRow) deriving (Show)
+
+data CachedTreeRow
+  = CachedTreeRow
+  { ctrEntryCount :: Word32
+  , ctrSubtreeCount :: Word32
+  , ctrSha1 :: Sha1
+  } deriving (Show)
 
 instance IndexExtension CachedTree where
   extSignature _ = "TREE"
-  extParser = undefined
+  extParser size = do
+      rows <- takeFor (fromIntegral size) entryP
+      return $ CachedTree $ Map.fromList rows
+    where
+      entryP = do
+        path <- Path.rel . Text.unpack <$> nullTermStringP
+        entryCount <- decimal
+        char_ ' '
+        subtreeCount <- decimal
+        char_ '\n'
+        sha1 <- sha1HexParser
+        return $ (path, CachedTreeRow entryCount subtreeCount sha1)
--- src/Git/Index/Extensions/ResolveUndo.hs
+++ src/Git/Index/Extensions/ResolveUndo.hs
@@ -1,9 +1,34 @@
 module Git.Index.Extensions.ResolveUndo where
 
-import Git.Index.Extensions.Class (IndexExtension(..))
+import Control.Monad (replicateM)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
+import qualified Data.Text as Text
+import qualified System.Path as Path
 
-data ResolveUndo = ResolveUndo deriving (Show)
+import Git.Index.Extensions.Class (IndexExtension(..))
+import Git.Index.Types (intToStage, Stages, mapToStages)
+import Git.Internal (takeFor, nullTermStringP, oct, char_, takeTill')
+import Git.Sha1 (Sha1, sha1HexParser)
+
+
+newtype ResolveUndo
+  = ResolveUndo (Map Path.RelFileDir (Stages Sha1)) deriving (Show)
 
 instance IndexExtension ResolveUndo where
   extSignature _ = "REUC"
-  extParser = undefined
+  extParser size = do
+      entries <- takeFor (fromIntegral size) entryP
+      return $ ResolveUndo $ Map.fromList entries
+    where
+      entryP = do
+        path <- Path.rel . Text.unpack <$> nullTermStringP
+        stages <- replicateM 3 (oct <* char_ '\NUL')
+          >>= mapM foo >>= mapToStages . Map.fromList . mapMaybe id
+        return (path, stages)
+      foo 0 = return Nothing
+      foo n = do
+        stage <- intToStage n
+        sha1 <- sha1HexParser
+        return $ Just (stage, sha1)
