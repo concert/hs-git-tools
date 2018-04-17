be7d12818d83e9846fb64c39c38187a182d93351
Paul Weaver <paul@concertdaw.co.uk>
2018-04-17 14:03:30 +0100

    Redefine IndexExtension class
    in an effort to make things more uniform. Perhaps it makes them more
    confusing?!

--- src/Git/Index/Extensions/Class.hs
+++ src/Git/Index/Extensions/Class.hs
@@ -2,27 +2,31 @@ module Git.Index.Extensions.Class where
 
 import Prelude hiding (take)
 
-import Control.Monad (replicateM, void)
-import Data.Attoparsec.ByteString (Parser, string, take)
+import Control.Monad (replicateM_, void)
+import Data.Attoparsec.ByteString (Parser, take, string)
 import Data.Attoparsec.ByteString.Char8 (satisfy)
 import Data.Attoparsec.Binary (anyWord32be)
 import qualified Data.ByteString as BS
 import Data.Char (isUpper)
 import Data.Proxy
+import Data.Word
 
 
 class IndexExtension a where
+  extSignatureParser :: Proxy a -> Parser ()
+  extSignatureParser p = void $ string $ extSignature p
+
   extSignature :: Proxy a -> BS.ByteString
-  extParser :: Parser a
+  extParser :: Word32 -> Parser a
 
 extensionP :: forall a. IndexExtension a => Parser a
 extensionP = do
-  _ <- string $ extSignature $ Proxy @a
-  _ <- anyWord32be
-  extParser
-
-voidExtensionP :: Parser ()
-voidExtensionP = do
-  _ <- replicateM 4 $ satisfy isUpper
+  extSignatureParser $ Proxy @a
   size <- anyWord32be
-  void $ take $ fromIntegral size
+  extParser size
+
+-- | An IndexExtension parser for an extension we don't recognise
+instance IndexExtension () where
+  extSignatureParser _ = replicateM_ 4 $ satisfy isUpper
+  extSignature _ = ""  -- Unused
+  extParser size = void $ take $ fromIntegral size
