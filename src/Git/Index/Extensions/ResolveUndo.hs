248cccd474fbe705cbdf132b8b2c7b7163a8bc43
Paul Weaver <paul@concertdaw.co.uk>
2018-04-20 17:55:28 +0100

    Implement ResolveUndo Index extension correctly
    At least, attempt to make it actually like the spec. Haven't tested yet!
    
    The crux is that the _modes_ are the octal values we read, and a _mode_ of
    zero implies there's no SHA1 to read. The _stage_ is actually positional,
    which makes far more sense!

--- src/Git/Index/Extensions/ResolveUndo.hs
+++ src/Git/Index/Extensions/ResolveUndo.hs
@@ -1,21 +1,26 @@
 module Git.Index.Extensions.ResolveUndo where
 
-import Control.Monad (replicateM)
+import Blaze.ByteString.Builder (fromByteString)
+import Control.Monad (replicateM, zipWithM)
+import qualified Data.ByteString.Char8 as Char8
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Maybe (mapMaybe)
+import Data.Maybe (catMaybes)
+import Data.Monoid
 import qualified System.Path as Path
+import Text.Printf (printf)
 
 import Git.Index.Extensions.Class
   (IndexExtension(..), BuildableIndexExtension(..))
-import Git.Index.Types (intToStage, Stages, mapToStages)
+import Git.Index.Types (Stages, mapToStages, stagesToMap)
 import Git.Internal (takeFor, nullTermStringP, oct, char_)
-import Git.Sha1 (Sha1, sha1HexParser)
+import Git.Sha1 (Sha1(..), sha1ByteStringParser)
+import Git.Types (FileMode, fileModeFromInt, fileModeToInt)
 
 
 newtype ResolveUndo
   = ResolveUndo
-  { unResolveUndo :: Map Path.RelFileDir (Stages Sha1)
+  { unResolveUndo :: Map Path.RelFileDir (Stages (FileMode, Sha1))
   } deriving (Show)
 
 instance IndexExtension ResolveUndo where
@@ -26,15 +31,24 @@ instance IndexExtension ResolveUndo where
     where
       entryP = do
         path <- Path.rel <$> nullTermStringP
-        stages <- replicateM 3 (oct <* char_ '\NUL')
-          >>= mapM foo >>= mapToStages . Map.fromList . mapMaybe id
+        modes <- replicateM 3 (oct <* char_ '\NUL')
+        stages <- zipWithM f [minBound..] modes >>=
+          mapToStages . Map.fromList . catMaybes
         return (path, stages)
-      foo 0 = return Nothing
-      foo n = do
-        stage <- intToStage n
-        sha1 <- sha1HexParser
-        return $ Just (stage, sha1)
+      f _ 0 = return Nothing
+      f stage i = do
+        mode <- fileModeFromInt i
+        sha1 <- sha1ByteStringParser
+        return $ Just (stage, (mode, sha1))
 
 instance BuildableIndexExtension ResolveUndo where
-  extBuilder = undefined
+  extBuilder = Map.foldMapWithKey f . fmap stagesToMap . unResolveUndo
+    where
+      b = fromByteString
+      p = b . Char8.pack . (++ "\n") . Path.toString
+      f path stages = p path <> modes stages <> sha1s stages
+      defaultModes = Map.fromList [(m, 0) | m <- [succ minBound..]]
+      modes stages = mconcat $ fmap (b . Char8.pack . printf "%o\NUL" . snd) $
+        Map.toAscList $ fmap (fileModeToInt . fst) stages <> defaultModes
+      sha1s = mconcat . fmap (b . unSha1 . snd . snd) . Map.toAscList
   extEmpty = Map.null . unResolveUndo
