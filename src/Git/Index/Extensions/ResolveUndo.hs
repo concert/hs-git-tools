9a0ed9a4c5a2af3a31cae1df12f034e9a53fb131
Paul Weaver <paul@concertdaw.co.uk>
2018-04-24 16:46:12 +0100

    Introduce a new ResolveUndo-specific Stages type

--- src/Git/Index/Extensions/ResolveUndo.hs
+++ src/Git/Index/Extensions/ResolveUndo.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE
+    DeriveFunctor
+#-}
+
 module Git.Index.Extensions.ResolveUndo where
+
+import Prelude hiding (fail)
 
 import Blaze.ByteString.Builder (fromByteString)
 import Control.Monad (replicateM, zipWithM)
+import Control.Monad.Fail (MonadFail(..))
+import Data.Attoparsec.ByteString (Parser)
 import qualified Data.ByteString.Char8 as Char8
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -12,7 +20,7 @@ import Text.Printf (printf)
 
 import Git.Index.Extensions.Class
   (IndexExtension(..), BuildableIndexExtension(..))
-import Git.Index.Types (Stages, mapToStages, stagesToMap)
+import Git.Index.Types (Stages(..), mapToStages, stagesToMap)
 import Git.Internal (takeFor, nullTermStringP, oct, char_)
 import Git.Sha1 (Sha1(..), sha1ByteStringParser)
 import Git.Types (FileMode, fileModeFromInt, fileModeToInt)
@@ -20,20 +28,44 @@ import Git.Types (FileMode, fileModeFromInt, fileModeToInt)
 
 newtype ResolveUndo
   = ResolveUndo
-  { unResolveUndo :: Map Path.RelFileDir (Stages (FileMode, Sha1))
+  { unResolveUndo :: Map Path.RelFileDir (ResolveUndoStages (FileMode, Sha1))
   } deriving (Show)
+
+data ResolveUndoStages a
+  = RuBothAdded {ruSsHead :: a, ruSsIncoming :: a}
+  | RuBothEdited {ruSsBase :: a, ruSsHead :: a, ruSsIncoming :: a}
+  | RuRmEdited {ruSsBase :: a, ruSsIncoming :: a}
+  | RuEditedRm {ruSsBase :: a, ruSsHead :: a}
+  deriving (Show, Functor)
+
+toStages :: ResolveUndoStages a -> Stages a
+toStages ruSs = case ruSs of
+  RuBothAdded hd inc -> BothAdded hd inc
+  RuBothEdited base hd inc -> BothEdited base hd inc
+  RuRmEdited base inc -> RmEdited base inc
+  RuEditedRm base hd -> EditedRm base hd
+
+fromStages :: MonadFail m => Stages a -> m (ResolveUndoStages a)
+fromStages ss = case ss of
+  Normal _ -> fail "Normal not an allowed ResolveUndo stage"
+  BothAdded hd inc -> return $ RuBothAdded hd inc
+  BothEdited base hd inc -> return $ RuBothEdited base hd inc
+  RmEdited base inc -> return $ RuRmEdited base inc
+  EditedRm base hd -> return $ RuEditedRm base hd
 
 instance IndexExtension ResolveUndo where
   extSignature _ = "REUC"
+  extEmpty = ResolveUndo mempty
   extParser size = do
       entries <- takeFor (fromIntegral size) entryP
       return $ ResolveUndo $ Map.fromList entries
     where
+      entryP :: Parser (Path.RelFileDir, ResolveUndoStages (FileMode, Sha1))
       entryP = do
         path <- Path.rel <$> nullTermStringP
         modes <- replicateM 3 (oct <* char_ '\NUL')
-        stages <- zipWithM f [minBound..] modes >>=
-          mapToStages . Map.fromList . catMaybes
+        stages <- zipWithM f [succ minBound..] modes >>=
+          mapToStages . Map.fromList . catMaybes >>= fromStages
         return (path, stages)
       f _ 0 = return Nothing
       f stage i = do
@@ -42,7 +74,7 @@ instance IndexExtension ResolveUndo where
         return $ Just (stage, (mode, sha1))
 
 instance BuildableIndexExtension ResolveUndo where
-  extBuilder = Map.foldMapWithKey f . fmap stagesToMap . unResolveUndo
+  extBuilder = Map.foldMapWithKey f . fmap (stagesToMap . toStages) . unResolveUndo
     where
       b = fromByteString
       p = b . Char8.pack . (++ "\n") . Path.toString
