0a7e345d7a904bee2d3ac75d700d13ed2b39d3d9
Paul Weaver <paul@concertdaw.co.uk>
2019-04-03 16:11:24 +0100

    Copy serialisation code
    in a manner that works with the new GADTs.

--- src/Git/Objects/Serialise.hs
+++ src/Git/Objects/Serialise.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE
-    MultiParamTypeClasses
+    DataKinds
+  , GADTs
+  , KindSignatures
+  , MultiParamTypeClasses
 #-}
 
 module Git.Objects.Serialise
@@ -28,8 +31,13 @@ import Text.Printf (printf)
 import Git.Internal (char_, lazyParseOnly)
 import Git.Sha1 (Sha1)
 import qualified Git.Sha1 as Sha1
+import Git.Objects.Blob (encodeBlob, blobParser)
+import Git.Objects.Commit (encodeCommit, commitParser)
 import Git.Objects.Object (Object(..))
 import Git.Objects.GitObject (GitObject(..), ObjectType(..))
+import Git.Objects.Internal (NewObject(..), SomeNewObject(..), nObjectType)
+import Git.Objects.Tag (encodeTag, tagParser)
+import Git.Objects.Tree (encodeTree, treeParser)
 
 encodeObjectType :: IsString str => ObjectType -> str
 encodeObjectType objTy = case objTy of
@@ -76,3 +84,57 @@ decodeLooseObject = lazyParseOnly (looseObjectParser <* endOfInput)
 decodeObject :: (MonadFail m, GitObject a) => BS.ByteString -> m a
 decodeObject bs = either fail return $ parseOnly
   (objectParser (fromIntegral $ BS.length bs) <* endOfInput) bs
+
+
+nEncodeObject :: NewObject t -> BS.ByteString
+nEncodeObject o = case o of
+  NObjBlob {} -> encodeBlob o
+  NObjTree {} -> encodeTree o
+  NObjCommit {} -> encodeCommit o
+  NObjTag {} -> encodeTag o
+
+nEncodeLooseObject :: NewObject t -> (Tagged t Sha1, BS.ByteString)
+nEncodeLooseObject o =
+  let
+    body = nEncodeObject o
+    encodedWithHeader =
+        encodeObjectType (nObjectType o) <> " "
+        <> (Char8.pack $ printf "%d" $ BS.length body)
+        <> "\NUL" <> body
+    -- FIXME: are we concerned about encoding large files using too much memory?
+    bodySha1 = Tagged $ Sha1.hashStrict encodedWithHeader
+  in
+    (bodySha1, encodedWithHeader)
+
+nGetObjectParser :: ObjectType -> Word64 -> Parser SomeNewObject
+nGetObjectParser objTy = case objTy of
+    ObjTyBlob -> f . blobParser
+    ObjTyTree -> f . treeParser
+    ObjTyCommit -> f . commitParser
+    ObjTyTag -> f . tagParser
+  where
+    f :: Parser (NewObject t) -> Parser SomeNewObject
+    f = fmap SomeNewObject
+
+nLooseObjectParser :: Parser SomeNewObject
+nLooseObjectParser = do
+  objTy <- objectTypeParser
+  size <- (char_ ' ' >> decimal <* char_ '\NUL') <?>  "object size"
+  nGetObjectParser objTy size
+
+class NewGitObject (t :: ObjectType) where
+  ngoParser :: Word64 -> Parser (NewObject t)
+
+instance NewGitObject ObjTyBlob where
+  ngoParser = blobParser
+instance NewGitObject ObjTyTree where
+  ngoParser = treeParser
+instance NewGitObject ObjTyCommit where
+  ngoParser = commitParser
+instance NewGitObject ObjTyTag where
+  ngoParser = tagParser
+
+nDecodeObject
+  :: (MonadFail m, NewGitObject t) => BS.ByteString -> m (NewObject t)
+nDecodeObject bs = either fail return $ parseOnly
+  (ngoParser (fromIntegral $ BS.length bs) <* endOfInput) bs
